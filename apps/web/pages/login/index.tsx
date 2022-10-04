2f2f98e060f5b4bf98c81c26df75dc2aa79935a5
zx <1345306401@qq.com>
2022-10-04 17:35:26 +0800

    feat: 登录注册布局

--- apps/web/pages/login/index.tsx
+++ apps/web/pages/login/index.tsx
@@ -1,31 +1,64 @@
-import { useRouter } from "next/router";
-import React, { FC, useEffect } from "react";
-import style from "./index.module.css";
-import { register } from "api-sdk";
-import { Form, Input, Button, Checkbox } from "@arco-design/web-react";
-
-const FormItem = Form.Item;
+import { useRouter, Router } from "next/router";
+import React, { ChangeEvent, FC, useState } from "react";
+import style from "./index.module.scss";
+import { login } from "api-sdk";
+import { Input, Button } from "@arco-design/web-react";
+import { setToken } from "../../utils/auth_token";
 
 const Login: FC = () => {
-  useEffect(() => {
-    console.log(
-      register().then((res) => {
-        console.log(res);
-      })
-    );
+  const router = useRouter();
+
+  const [info, setInfo] = useState<{
+    account: string;
+    password: string;
+  }>({
+    account: "",
+    password: "",
   });
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleChange = (value: string, e: ChangeEvent<HTMLInputElement>) => {
+    const { name } = e.target;
+    const data = { ...info, [name]: value };
+    setInfo(data);
+  };
+
+  const handleLogin = () => {
+    setLoading(true);
+    login().then((res) => {
+      console.log(res);
+      setToken(res.data.token);
+      router.push("/");
+      setLoading(false);
+    });
+  };
 
   return (
     <div className={style.login}>
-      <div className="style.form">
+      <div className={style.form}>
+        <h2 className={style.title}>login</h2>
         <div>
-          <Input placeholder="请输入账号、手机号、邮箱" />
+          <Input
+            placeholder="请输入账号、手机号、邮箱"
+            name="account"
+            onChange={handleChange}
+          />
         </div>
         <div>
-          <Input.Password placeholder="请输入密码" />
+          <Input.Password
+            placeholder="请输入密码"
+            name="password"
+            onChange={handleChange}
+          />
+          <div className={style.find}>
+            <span onClick={() => router.push("/register")}>注册!</span>
+            <span>忘记密码?</span>
+          </div>
         </div>
         <div>
-          <Button>登录</Button>
+          <Button onClick={handleLogin} loading={loading}>
+            登录
+          </Button>
         </div>
       </div>
     </div>
