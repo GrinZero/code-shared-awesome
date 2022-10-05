02e6b830a2b806cc82324efcc623ee45b0444a4a
zx <1345306401@qq.com>
2022-10-05 21:26:06 +0800

    feat: personal 内容list

--- apps/web/pages/personal/index.tsx
+++ apps/web/pages/personal/index.tsx
@@ -4,15 +4,17 @@ import { Menu } from "@arco-design/web-react";
 import MainLayout from "../../layouts/main-layout";
 import Achievement from "./Achievement";
 import AvatarHead from "./AvatarHead";
-import { getUserInfo } from "api-sdk";
+import { getArticleList, getUserInfo } from "api-sdk";
+import ArticleList from "../../components/articleList";
+import { ListDataProps } from "../../types";
 
 const MenuItem = Menu.Item;
 
 const menus = [
-  { title: "动态", key: "1" },
-  { title: "发布", key: "2" },
-  { title: "关注", key: "3" },
-  { title: "专栏", key: "4" },
+  { title: "动态", key: "0" },
+  { title: "发布", key: "1" },
+  { title: "关注", key: "2" },
+  { title: "专栏", key: "3" },
 ];
 
 export interface UserInfo {
@@ -35,6 +37,7 @@ const Personal: React.FC = () => {
     name: "",
     account: "",
   });
+  const [listData, setListData] = useState<ListDataProps[]>();
 
   const menuElements = useMemo(() => {
     const menuStyle = { backgroundColor: "var(--theme-bg-color)" };
@@ -62,6 +65,9 @@ const Personal: React.FC = () => {
     getUserInfo().then((res) => {
       res.status === "ok" && setUserInfo(res.data);
     });
+    getArticleList().then((res) => {
+      res.status === "ok" && setListData(res.data);
+    });
   }, []);
 
   return (
@@ -72,7 +78,14 @@ const Personal: React.FC = () => {
 
           <div className={style.menuDemo}>{menuElements}</div>
 
-          <div className={style.content}></div>
+          <div className={style.content}>
+            {listData && (
+              <ArticleList
+                listData={listData}
+                activeIndex={parseInt(selectMenu)}
+              />
+            )}
+          </div>
         </div>
         <div className={style.right}>
           <Achievement userInfo={userInfo} />
