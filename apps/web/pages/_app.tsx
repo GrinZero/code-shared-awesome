d2cbac1dbbbc2fe357968c288611947e07900858
zx <1345306401@qq.com>
2022-10-06 20:51:03 +0800

    style: personal页面移动端适配

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -16,6 +16,7 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>Code Show</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component />
