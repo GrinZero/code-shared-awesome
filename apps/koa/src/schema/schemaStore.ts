e544d44f7facc06877af765b69533ded6b165dd3
bugyalu <774933704@qq.com>
2022-10-07 16:42:24 +0800

    refactor(koa): delete schema type

--- apps/koa/src/schema/schemaStore.ts
+++ apps/koa/src/schema/schemaStore.ts
@@ -1,15 +1,7 @@
 import { GraphQLObjectType as GObject } from "graphql";
 import type { GraphQLFieldConfig, GraphQLObjectTypeConfig } from "graphql";
 
-export type SchemaStoreType<T, S> = {
-  add: (name: string, config: GraphQLFieldConfig<any, any>) => void;
-  finish: (
-    config: Readonly<Omit<GraphQLObjectTypeConfig<T, S>, "fields">>
-  ) => GObject<T, S>;
-  fields: Record<string, GraphQLFieldConfig<any, any, any>>;
-};
-
-export class SchemaStore<T = any, S = any> implements SchemaStoreType<T, S> {
+export class SchemaStore<T = any, S = any> {
   fields: Record<string, GraphQLFieldConfig<any, any, any>> = {};
   add(name: string, field: GraphQLFieldConfig<any, any, any>) {
     this.fields[name] = field;
@@ -19,7 +11,7 @@ export class SchemaStore<T = any, S = any> implements SchemaStoreType<T, S> {
   }
 }
 
-export const mergeSchemaStore = (...stores: SchemaStoreType<any, any>[]) => {
+export const mergeSchemaStore = (...stores: SchemaStore<any, any>[]) => {
   const store = new SchemaStore();
   stores.forEach((s) => {
     store.fields = { ...store.fields, ...s.fields };
