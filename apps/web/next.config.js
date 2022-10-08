b987145bcffa00cc5a1a99bf47551e82b8b8f083
bugyalu <774933704@qq.com>
2022-10-08 22:56:58 +0800

    ci(web): assetPrefix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,4 +3,5 @@ const withTM = require("next-transpile-modules")(["ui", "api-sdk"]);
 module.exports = withTM({
   reactStrictMode: true,
   basePath: "/code-shared-awesome",
+  assetPrefix: "/code-shared-awesome/",
 });
